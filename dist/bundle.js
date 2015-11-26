e8e097990f94ea3f12ec83de5ffceada2b3af614
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-26 09:05:07 -0800

    Allow in-line tag name editing (not synced with API yet)

--- dist/bundle.js
+++ dist/bundle.js
@@ -43,6 +43,39 @@ app.controller('homeCtrl', ['$scope', '$http', 'API', function ($scope, $http, A
   }).catch(function (err) {
     console.error(err);
   });
+
+  $scope.edit = function (tag) {
+    console.log(event.target);
+    var $org = $(event.target);
+    var $fld = $('<input class="fld">');
+    $fld.val(tag.name);
+    $fld.width($org.width());
+    $fld.height($org.height());
+
+    var paddingTop = parseFloat($org.css('padding-top'));
+    var paddingLeft = parseFloat($org.css('padding-left'));
+
+    var pos = $org.position();
+    $fld.css('top', pos.top + paddingTop);
+    $fld.css('left', pos.left + paddingLeft);
+
+    $org.append($fld);
+    $fld.focus();
+    $fld.select();
+  };
+
+  $scope.key = function () {
+    console.log(event.which);
+    if (event.which === 13) {
+      var newval = $(event.target).val();
+      $(event.target).parent().text(newval + ', ');
+
+      $(event.target).remove();
+      console.log(newval);
+    } else if (event.which === 27) {
+      $(event.target).remove();
+    }
+  };
 }]);
 
 'use strict';
